9992a0135459edff27f9f457719a573edfddf3c3
mrdale1958 <dale.macdonald@utdallas.edu>
2020-04-04 11:55:21 -0500

    restored installation version reconfigured css for web

--- installation/kitchen.js
+++ installation/kitchen.js
@@ -20,7 +20,7 @@ fetch('epickitchens_clips_db.txt')
 		sortVideoFilesByVerbs(videofiles);
 	});
 
-fetch('epic-hand-washing-in-the-time-of-lost-narrative-march23.txt')
+fetch('Epic-Kitchen-Ones-Own-Database.txt')
 	.then((response) => {
 		return response.text();
 	})
@@ -32,10 +32,9 @@ fetch('epic-hand-washing-in-the-time-of-lost-narrative-march23.txt')
 
 	});
 
-
 //let database = readCSVfile('epic-kitchen-ones-own.csv');
-//console.log("epic-kitchen-ones-own.csv");
-//console.log(database);
+console.log("epic-kitchen-ones-own.csv");
+console.log(database);
 
 let OneOnFourUp = [
 
@@ -121,27 +120,12 @@ let HorizontalLine = [
 					{ "class": "div-1", "size": "1up" }
 				];
 
-let justCenter4up = [
-						{ "class": "center-half-vid", "size": "4up" }
-				];
-
-let bgCenter2up = [
-						{ "class": "", "size": "2up" }
-				];
-
 let videoDivsBySize = 
 					{ "_1up" : '<video width="3072" height="2732"   loop  muted >\n',
 					 "_2up" : '<video width="1536" height="1366"   loop  muted >\n',
 					 "_4up" : '<video width="768" height="684"   loop  muted >\n',
 					 "_8up" : '<video width="384" height="342"   loop  muted >\n'};
 
-let vimeoDivsBySize = 
-{
-	"_4up" : "<div class='center-center-vid' ><iframe src='https://player.vimeo.com/video/xxxxxxxxxx?loop=true&autoplay=1&autopause=0&muted=true&byline=false&portrait=false&title=false&controls=false'"  + 
-				" width='921' height='821' frameborder='0' ></iframe></div>" ,
-	"_2up" : "<div id='bgvideo' ><iframe src='https://player.vimeo.com/video/xxxxxxxxxx?loop=true&autoplay=1&autopause=0&muted=true&byline=false&portrait=false&title=false&controls=false'"  + 
-				" width='1536' height='1366' frameborder='0' ></iframe></div>" 
-}
 /*let videoDivsBySize = 
 					{ "_1up" : '<video width="3072" height="2732"  autoplay loop  muted >\n',
 					 "_2up" : '<video width="1536" height="1366"  autoplay loop  muted >\n',
@@ -149,7 +133,10 @@ let vimeoDivsBySize =
 					 "_8up" : '<video width="384" height="342"  autoplay loop  muted >\n'};
 */					
 
-let layouts = [ bgCenter2up ];
+let layouts = [ OneOnFourUp, TwoOnFourUp, FourUp, EightUp, 
+				EightDown, FourOnFour, // KitchenSink, 
+				XbeX, VerticalLine,  HorizontalLine, 
+				XbeXFloat, VerticalLineFloat,  HorizontalLineFloat ];
 let queuedVideos = [];
 let videosToQueue = 0;
 let readyToDisplay = false;
@@ -175,7 +162,7 @@ function generateTopBottomLayouts() {
 	}
 	return topBottomLayouts;
 }
-//layouts = layouts.concat(generateTopBottomLayouts());
+layouts = layouts.concat(generateTopBottomLayouts());
 
 function generateSparseTopLayouts(count) {
 	var sparseTopLayouts = [];
@@ -205,7 +192,7 @@ function generateSparseTopLayouts(count) {
 	}
 	return sparseTopLayouts;
 }
-//layouts = layouts.concat(generateSparseTopLayouts(100));
+layouts = layouts.concat(generateSparseTopLayouts(100));
 
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
@@ -218,7 +205,6 @@ function getParameterByName(name, url) {
 }
 
 let nextLine = parseInt(getParameterByName('start'));
-let videoSource = getParameterByName('videosource');
 let sortedByVerbs = {}
 let usedByVerbs = {}
 
@@ -228,7 +214,6 @@ function sortVideoFilesByVerbs(videofiles) {
 		let currentVerb = currentVideo['verb']
 		if (! (currentVerb in sortedByVerbs)) {
 			sortedByVerbs[currentVerb] = [currentVideo];
-			usedByVerbs[currentVerb] = [];
 		} else {
 			sortedByVerbs[currentVerb].push(currentVideo);
 		}
@@ -254,60 +239,41 @@ function chooseLine() {
 
 function chooseVideos(count, verb) {
 	let videoList = [];
-	if ( verb == undefined) {
-		for (let i=0; i < count; i++) {
+	for (let i=0; i < count; i++) {
+		if ( verb == undefined) {
 			let candidate_video = videofiles[Math.floor(Math.random()*videofiles.length)];
 			videoList.push(candidate_video['filename']);
+
 		}
-	}
-	else  {
-		if (sortedByVerbs[verb].length < count) {
-				// all videos for verb have been played, reset the list
+		else {
+			if (sortedByVerbs[verb].length < count) {
 				sortedByVerbs[verb] = sortedByVerbs[verb].concat(usedByVerbs[verb]);
 				usedByVerbs[verb] = [];
 			}
 			for (let j=0; j<count;j++) {
-				let sourceIndex = Math.floor(Math.random()*sortedByVerbs[verb].length);
-				let candidate_video = sortedByVerbs[verb].splice(sourceIndex,1)[0];
-				usedByVerbs[verb].push (candidate_video);
-				if (videoSource != 'vimeo')
-					videoList.push(candidate_video['filename']);
-				else
-					videoList.push(candidate_video['vimeoName']);
-
+				let candidate_video = sortedByVerbs[verb][Math.floor(Math.random()*videofiles.length)];
+				videoList.push(candidate_video['filename']);
 
 			}
 		}
-	return videoList;
+		return videoList;
+	//}
+
 }
 
 function populateTextDiv(text) { 
-	let pClass = "";
-	//if (text.length > 100) pClass = 'class="smaller"';
-
-	//let thisdiv = '<p ' + pClass + '>' + text + '</p>';
-	let thisdiv = text;
+	let thisdiv = '<p>' + text + '</p>';
 	return thisdiv ; 
 }
 
 function populateVideoDivs(videoList, layout) {
 	let returndivs = "";
-	if (videoSource != 'vimeo') {
-		for (const newdiv in layout) {
-			let nextVideo = videoList[Math.floor(Math.random()*videoList.length)];
-			returndivs += '<div class="delayed-video ' + layout[newdiv]['class'] + '">\n'
-			returndivs += videoDivsBySize['_' + layout[newdiv]['size']];
-	  		returndivs += '<source src="clips/timed_30/' + nextVideo + '_' 
-	  			+ layout[newdiv]['size'] + '.MP4" type="video/mp4">\n</video>\n</div>\n';
-		}
-	} else {
-		for (const newdiv in layout) {
-			let nextVideo = videoList[Math.floor(Math.random()*videoList.length)];
-			returndivs += '<div class="delayed-video ' + layout[newdiv]['class'] + '">\n'
-			returndivs += vimeoDivsBySize['_' + layout[newdiv]['size']];
-	  		returndivs += '\n</div>\n';
-	  		returndivs = returndivs.replace("xxxxxxxxxx", nextVideo);
-		}
+	for (const newdiv in layout) {
+		let nextVideo = videofiles[Math.floor(Math.random()*videofiles.length)];
+		returndivs += '<div class="delayed-video ' + layout[newdiv]['class'] + '">\n'
+		returndivs += videoDivsBySize['_' + layout[newdiv]['size']];
+  		returndivs += '<source src="clips/timed_30/' + nextVideo['filename'] + '_' 
+  			+ layout[newdiv]['size'] + '.MP4" type="video/mp4">\n</video>\n</div>\n';
 	}
 	return returndivs;
 }
@@ -324,16 +290,16 @@ function fadeText() {
 	let textdiv = document.getElementById('quote-text');
 	textdiv.classList.toggle('m-fadeIn');
 	textdiv.classList.toggle('m-fadeOut');
-	//let tagdiv = document.getElementById('verb-text');
-	//tagdiv.classList.toggle('m-fadeIn');
-	//tagdiv.classList.toggle('m-fadeOut');
+	let tagdiv = document.getElementById('verb-text');
+	tagdiv.classList.toggle('m-fadeIn');
+	tagdiv.classList.toggle('m-fadeOut');
 	setTimeout(runExhibit, 3000);
 }
 
 function enableNewPage() {
 	//return "almost implemented"
 	let textdiv = document.getElementById('quote-text');
-	//let tagdiv = document.getElementById('verb-text');
+	let tagdiv = document.getElementById('verb-text');
 	//textdiv.classList.remove('div-top-fade-in');
 	//textdiv.classList.add('div-top-fade-out');
 	//tagdiv.classList.remove('div-top-fade-in');
@@ -344,31 +310,31 @@ function enableNewPage() {
 		activeVideo.parentNode.removeChild(activeVideo);
 	}
 	let delayedVideoDivs = document.getElementsByClassName("delayed-video");
-	for (var activatingVideoDiv = 0;  activatingVideoDiv < delayedVideoDivs.length; activatingVideoDiv++) {
-		delayedVideoDivs[activatingVideoDiv].classList.add("active-video");
+	for (var delayedVideoDiv = 0;  delayedVideoDiv < delayedVideoDivs.length; delayedVideoDiv++) {
+		delayedVideoDivs[delayedVideoDiv].classList.add("active-video");
 	}
 	for (var delayedVideoDiv = 0;  delayedVideoDiv < delayedVideoDivs.length; delayedVideoDiv++) {
 		delayedVideoDivs[delayedVideoDiv].classList.remove("delayed-video");
 	}
 	activeVideoDivs = document.getElementsByClassName("active-video");
-	for (var activatedVideoDiv = 0;  activatedVideoDiv < activeVideoDivs.length; activatedVideoDiv++) {
-		let newVideoDiv = activeVideoDivs[activatedVideoDiv].getElementsByTagName('video')[0];
+	for (var activeVideoDiv = 0;  activeVideoDiv < activeVideoDivs.length; activeVideoDiv++) {
+		let newVideoDiv = activeVideoDivs[activeVideoDiv].getElementsByTagName('video')[0];
 		newVideoDiv.play();
 	}
 	//textdiv.classList.add('div-top-fade-in');
 	//textdiv.classList.remove('div-top-fade-out');
-	textdiv.classList.remove('short','middle','long');
+	textdiv.classList.remove('literature','journalism','socialmedia');
 	textdiv.classList.add(datum['Type']);
-	//textdiv.innerHTML = populateTextDiv(datum['Quote'] );
-	//textdiv.classList.toggle('m-fadeOut');
-	//textdiv.classList.toggle('m-fadeIn');
+	textdiv.innerHTML = populateTextDiv(datum['Quote'] );
+	textdiv.classList.toggle('m-fadeOut');
+	textdiv.classList.toggle('m-fadeIn');
 	//tagdiv.classList.add('div-top-fade-in');
 	//tagdiv.classList.remove('div-top-fade-out');
-	//tagdiv.classList.remove('literature-tag','journalism-tag','socialmedia-tag');
-	//tagdiv.classList.add(datum['Type'] + '-tag');
-	//tagdiv.innerHTML = '#' + datum['Verb'] ;
-	//tagdiv.classList.toggle('m-fadeOut');
-	//tagdiv.classList.toggle('m-fadeIn');
+	tagdiv.classList.remove('literature-tag','journalism-tag','socialmedia-tag');
+	tagdiv.classList.add(datum['Type'] + '-tag');
+	tagdiv.innerHTML = '#' + datum['Verb'] ;
+	tagdiv.classList.toggle('m-fadeOut');
+	tagdiv.classList.toggle('m-fadeIn');
 		
 }
 
@@ -383,17 +349,10 @@ function runExhibit(database) {
 	let delayedVideoDivs = document.getElementsByClassName("delayed-video");
 	for (var delayedVideoDiv = 0;  delayedVideoDiv < delayedVideoDivs.length; delayedVideoDiv++) {
 		let newVideoDiv = delayedVideoDivs[delayedVideoDiv].getElementsByTagName('video')[0];
-		//newVideoDiv.oncanplaythrough = enqueueVideo ; 
+		newVideoDiv.oncanplaythrough = enqueueVideo ; 
+		let foo = 0;
 	}
-	let textdiv = document.getElementById('quote-text');
-	textdiv.classList.remove('short','middle','long');
-	textdiv.classList.add(datum['Type']);
-	textdiv.innerHTML = populateTextDiv(datum['Quote'] );
-	textdiv.setAttribute("alt", datum['Title']);
-	textdiv.setAttribute("title", datum['Title']);
-	//if (datum['Quote'].length > 100) textdiv.classList.add('smaller');
-	textdiv.classList.toggle('m-fadeOut');
-	textdiv.classList.toggle('m-fadeIn');
-		setTimeout(fadeText, 27000);
+	setTimeout(fadeText, 30000);
 }
+
 
